deb9f934eead04ad1c2d6a6c64b48b93a3d915ba
Igor Stepanov <iystepanov@mts.ru>
2024-08-26 23:12:23 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String,String> phones = new HashMap<>();
 
     public int add(String name, String number) {
-        return 0;
+        if (phones.containsKey(name)) {
+            System.out.println("Это имя уже есть в телефонной книге");
+        } else {
+        phones.put(name,number);
+        }
+        return phones.size();
     }
 }
