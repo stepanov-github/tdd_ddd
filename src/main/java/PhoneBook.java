2a71a7181b0bcc04ce74957c5a9abeb9fc5531e4
Igor Stepanov <iystepanov@mts.ru>
2024-08-27 02:35:35 +0300

    printAllNames implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class PhoneBook {
     private Map<String,String> phones = new HashMap<>();
@@ -26,7 +29,15 @@ public class PhoneBook {
         return phones.get(name);
     }
 
-    public String printAllNames() {
-        return null;
+    public void printAllNames() {
+        List<String> keys =new ArrayList<>();
+        for (Map.Entry<String,String> entry:phones.entrySet()) {
+            keys.add(entry.getKey());
+        }
+
+        Stream stream = keys.stream();
+        stream
+                .sorted()
+                .forEach(System.out::println);
     }
 }
