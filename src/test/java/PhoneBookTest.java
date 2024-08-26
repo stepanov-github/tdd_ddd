2b4e3deadbda99bf6636cfc02c087198c61a332f
Igor Stepanov <iystepanov@mts.ru>
2024-08-26 22:47:17 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testadd() {
+
+        // given:
+        PhoneBook phoneBook = new PhoneBook();
+        int expected = 1;
+
+        // when:
+        int count = phoneBook.add("Petya", "4951112233");
+
+        // then:
+        Assertions.assertEquals(expected,count);
+    }
 }
